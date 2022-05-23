3736b292ec6228ed2a168421df11f8f919d968ca
mwysam <samuelmwy@gmail.com>
2022-05-23 11:00:04 -0700

    gather data

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Gather data from API
+"""
+
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    user_id = int(sys.argv[1])
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+            .format(user_id))
+    user_name = user.json().get('user_name')
+
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos/")
+
+    for task in todos.json():
+        if task.get("user_id") == (user_id):
+            total_tasks += 1
+            if task.get("tasks_done")
+            tasks_done += 1
+
+print(
+   "Employee {} is done with tasks ({}/{})"
+    .format(user_name,tasks_done,total_tasks)
+       
+    "/t {}".format(tasks_done.get('title'))
+    )
