08f038c3747dde56adbc9b62fc68907fccb56f52
mwysam <samuelmwy@gmail.com>
2022-05-23 11:14:48 -0700

    gather edit

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,21 +8,25 @@ import sys
 
 
 if __name__ == '__main__':
+
     user_id = int(sys.argv[1])
     user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
             .format(user_id))
+
     user_name = user.json().get('user_name')
 
     todos = requests.get("https://jsonplaceholder.typicode.com/todos/")
-
+    tasks_done = 0
+    total_tasks = 0
+    
     for task in todos.json():
         if task.get("user_id") == (user_id):
             total_tasks += 1
-            if task.get("tasks_done")
+            if task.get("tasks_done"):
             tasks_done += 1
 
 print(
-   "Employee {} is done with tasks ({}/{})"
+   "Employee {} is done with tasks ({}/{}):"
     .format(user_name,tasks_done,total_tasks)
        
     "/t {}".format(tasks_done.get('title'))
