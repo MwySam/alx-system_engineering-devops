6b477c22ba23493ad3abda789a51c36686e3df6b
mwysam <samuelmwy@gmail.com>
2022-06-16 14:08:55 -0700

    bracket fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,4 @@
 # Increase limit of requests to nginx server
-
 exec { 'fix--for-nginx':
   command => "sed -i 's/15/4096/' /etc/default/nginx; sudo service nginx restart",
   path    => ['/usr/local/bin/:/bin/', '/usr/sbin']
